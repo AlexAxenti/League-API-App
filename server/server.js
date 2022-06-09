ae1ade3dcf5cb4001ddd4f5ad24ee55d4dd75536
AlexAxenti <alex.axenti@gmail.com>
2022-06-09 11:51:11 -0400

    Setting up build for heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 7000
 
 var indexRouter = require('./routes/index');
 var cors = require("cors");
 
-app.use(cors());
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+//app.use(cors());
 app.use('/api', indexRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
