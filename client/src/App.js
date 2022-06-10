93568ab134a82628f53e43e13b0561193a96dbd3
Carl Paladino <carlpaladino9@hotmail.com>
2022-06-10 01:04:25 -0400

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ export default function App() {
           <a className="navbar-brand" href="/">Home</a>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/summoners">Summoners <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/summoners">Summoners<span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/about">About</a>
